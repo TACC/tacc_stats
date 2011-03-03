893a7a53048a641ddf1cdc46f31f05f66525e7bd
John L. Hammond <jhammond@ices.utexas.edu>
2011-03-03 16:06:52 -0600

    Move ST_IB to /sys.

--- st_ib.c
+++ st_ib.c
@@ -2,89 +2,114 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <dirent.h>
 #include "stats.h"
+#include "collect.h"
 #include "trace.h"
 
-const char *ib_stats_cmd = "/opt/ofed/sbin/perfquery -r";
+/* Fields for mlx4, mthca: */
 
-// $ perfquery
-// # Port counters: Lid 936 port 1
-// PortSelect:......................1
-// CounterSelect:...................0x0000
-// SymbolErrors:....................0
-// LinkRecovers:....................0
-// LinkDowned:......................0
-// RcvErrors:.......................0
-// RcvRemotePhysErrors:.............0
-// RcvSwRelayErrors:................0
-// XmtDiscards:.....................0
-// XmtConstraintErrors:.............0
-// RcvConstraintErrors:.............0
-// LinkIntegrityErrors:.............0
-// ExcBufOverrunErrors:.............0
-// VL15Dropped:.....................0
-// XmtData:.........................905972985
-// RcvData:.........................2411533950
-// XmtPkts:.........................22945597
-// RcvPkts:.........................26264095
+#define IB_KEYS \
+  X(excessive_buffer_overrun_errors), \
+  X(link_downed), \
+  X(link_error_recovery), \
+  X(local_link_integrity_errors), \
+  X(port_rcv_constraint_errors), \
+  X(port_rcv_data), \
+  X(port_rcv_errors), \
+  X(port_rcv_packets), \
+  X(port_rcv_remote_physical_errors), \
+  X(port_rcv_switch_relay_errors), \
+  X(port_xmit_constraint_errors), \
+  X(port_xmit_data), \
+  X(port_xmit_discards), \
+  X(port_xmit_packets), \
+  X(port_xmit_wait), \
+  X(symbol_error), \
+  X(VL15_dropped)
 
-static void collect_ib_stats(struct stats_type *type)
+static void collect_ib_dev(struct stats_type *type, const char *dev)
 {
-  struct stats *ib_stats = NULL;
-  FILE *pipe = NULL;
-  char *line = NULL;
-  size_t line_size = 0;
+  int port;
+  for (port = 1; port <= 2; port++) {
+    char path[80], id[80], cmd[160];
+    FILE *file = NULL;
+    unsigned int lid;
+    struct stats *stats = NULL;
 
-  pipe = popen(ib_stats_cmd, "r");
-  if (pipe == NULL) {
-    ERROR("cannot execute `%s': %m\n", ib_stats_cmd);
+    snprintf(path, sizeof(path), "/sys/class/infiniband/%s/ports/%i/state", dev, port);
+    file = fopen(path, "r");
+    if (file == NULL)
+      goto next; /* ERROR("cannot open `%s': %m\n", path); */
+
+    char buf[80] = { 0 };
+    if (fgets(buf, sizeof(buf), file) == NULL)
+      goto next;
+
+    fclose(file);
+    file = NULL;
+
+    if (strstr(buf, "ACTIVE") == NULL)
+      goto next;
+
+    TRACE("dev %s, port %i\n", dev, port);
+
+    snprintf(id, sizeof(id), "%s.%i", dev, port); /* XXX */
+    stats = get_current_stats(type, id);
+    if (stats == NULL)
+      goto next;
+
+    snprintf(path, sizeof(path), "/sys/class/infiniband/%s/ports/%i/counters", dev, port);
+    collect_key_value_dir(stats, path);
+
+    /* Get the LID for perfquery. */
+    snprintf(path, sizeof(path), "/sys/class/infiniband/%s/ports/%i/lid", dev, port);
+    file = fopen(path, "r");
+    if (fscanf(file, "%x", &lid) != 1)
+      goto next;
+
+    fclose(file);
+    file = NULL;
+
+    /* Call perfquery to clear stats.  Blech! */
+    snprintf(cmd, sizeof(cmd), "/opt/ofed/sbin/perfquery -R %#x %d", lid, port);
+    TRACE("system `%s'\n", cmd);
+    system(cmd);
+
+  next:
+    if (file != NULL)
+      fclose(file);
+    file = NULL;
+  }
+}
+
+static void collect_ib(struct stats_type *type)
+{
+  const char *path = "/sys/class/infiniband";
+  DIR *dir = NULL;
+
+  dir = opendir(path);
+  if (dir == NULL) {
+    ERROR("cannot open `%s': %m\n", path);
     goto out;
   }
 
-  ib_stats = get_current_stats(type, NULL); /* Use port number as id? */
-  if (ib_stats == NULL)
-    goto out;
-
-  while (getline(&line, &line_size, pipe) >= 0) {
-    char *key, *rest = line;
-    unsigned long long val;
-
-    /* Older versions of perfquery write errors to stdout.  To make
-       sure we're not parsing error messages, we check that the line
-       has the goofy KEY:......VALUE format. */
-
-    key = strsep(&rest, ":");
-    if (*key == 0 || rest == NULL)
+  struct dirent *ent;
+  while ((ent = readdir(dir)) != NULL) {
+    if (ent->d_name[0] == '.')
       continue;
-
-    if (*rest != '.')
-      continue;
-
-    while (*rest == '.')
-      rest++;
-
-    errno = 0;
-    val = strtoull(rest, NULL, 0);
-    if (errno != 0)
-      continue;
-
-    /* Data values are counted in units of 4 octets. */
-    if (strcmp(key, "XmtData") == 0 || strcmp(key, "RcvData") == 0)
-      val *= 4;
-
-    stats_set(ib_stats, key, val);
+    collect_ib_dev(type, ent->d_name);
   }
 
  out:
-  free(line);
-  if (pipe != NULL)
-    pclose(pipe);
+  if (dir != NULL)
+    closedir(dir);
 }
 
 struct stats_type ST_IB_TYPE = {
   .st_name = "ST_IB",
-  .st_collect = (void (*[])()) { &collect_ib_stats, NULL, },
-  .st_schema = (char *[]) {
-    "RcvPkts", "XmtPkts", "RvcData", "XmtData", NULL,
-  },
+  .st_collect = (void (*[])()) { &collect_ib, NULL, },
+#define X(K) #K
+  .st_schema = (char *[]) { IB_KEYS, NULL, },
+#undef X
 };
