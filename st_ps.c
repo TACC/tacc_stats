78ca2f653a275806693857d604a126969e831169
John L. Hammond <jhammond@ices.utexas.edu>
2011-03-05 10:34:21 -0600

    Add collect_ps().

--- st_ps.c
+++ st_ps.c
@@ -9,11 +9,20 @@
 #include "stats.h"
 #include "trace.h"
 
-static void collect_proc_stat(struct stats_type *type)
+// $ cat /proc/stat
+// cpu ...
+// ...
+// intr ...
+// ctxt 15088509272
+// btime 1288194676
+// processes 2591587 /* nr_forks */
+// procs_running 17
+// procs_blocked 0
+
+static void collect_proc_stat(struct stats *ps_stats)
 {
   const char *path = "/proc/stat";
   FILE *file = NULL;
-  struct stats *ps_stats = NULL;
   char *line = NULL;
   size_t line_size = 0;
 
@@ -22,10 +31,6 @@ static void collect_proc_stat(struct stats_type *type)
     ERROR("cannot open `%s': %m\n", path);
     goto out;
   }
-
-  ps_stats = get_current_stats(type, NULL);
-  if (ps_stats == NULL)
-    goto out;
 
   while (getline(&line, &line_size, file) >= 0) {
     char *key, *rest = line;
@@ -51,21 +56,16 @@ static void collect_proc_stat(struct stats_type *type)
     fclose(file);
 }
 
-static void collect_loadavg(struct stats_type *type)
+static void collect_loadavg(struct stats *ps_stats)
 {
   const char *path = "/proc/loadavg";
   FILE *file = NULL;
-  struct stats *ps_stats = NULL;
 
   file = fopen(path, "r");
   if (file == NULL) {
     ERROR("cannot open `%s': %m\n", path);
     goto out;
   }
-
-  ps_stats = get_current_stats(type, NULL);
-  if (ps_stats == NULL)
-    goto out;
 
   unsigned long long load[3][2];
   unsigned long long nr_running = 0, nr_threads = 0;
@@ -93,20 +93,22 @@ static void collect_loadavg(struct stats_type *type)
     fclose(file);
 }
 
-// $ cat /proc/stat
-// cpu ...
-// ...
-// intr ...
-// ctxt 15088509272
-// btime 1288194676
-// processes 2591587
-// procs_running 17
-// procs_blocked 0
+static void collect_ps(struct stats_type *type)
+{
+  struct stats *ps_stats = NULL;
+
+  ps_stats = get_current_stats(type, NULL);
+  if (ps_stats == NULL)
+    return;
+
+  collect_proc_stat(ps_stats);
+  collect_loadavg(ps_stats);
+}
 
 struct stats_type ST_PS_TYPE = {
   .st_name = "ST_PS",
-  .st_collect = (void (*[])()) { &collect_proc_stat, &collect_loadavg, NULL, },
+  .st_collect = (void (*[])()) { &collect_ps, NULL, },
   .st_schema = (char *[]) {
-    "btime", "ctxt", "processes", "load_1", "load_5", "load_15", NULL,
+    "btime", "ctxt", "processes", "load_1", "load_5", "load_15", "nr_running", "nr_threads", NULL,
   },
 };
