7d247989b60f26cbda43d4b2dfa34b2b84c3b4a5
John L. Hammond <jhammond@ices.utexas.edu>
2011-03-21 17:18:30 -0500

    Rewrite st_block.c to use collect_key_list().

--- st_block.c
+++ st_block.c
@@ -1,57 +1,33 @@
 #define _GNU_SOURCE
 #include <stddef.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include <string.h>
 #include <dirent.h>
-#include <errno.h>
-#include <malloc.h>
-#include <ctype.h>
 #include "stats.h"
+#include "collect.h"
 #include "trace.h"
 
 /* Need to account for units.  According to block/stat.txt, in
 /sys/block/DEV/stat sector means 512B (as opposed to real sector size
 of device). */
+/* All X_ticks members and time_in_queue are in ms. */
+
+#define BLOCK_KEYS \
+  X(rd_ios), X(rd_merges), X(rd_sectors), X(rd_ticks), \
+  X(wr_ios), X(wr_merges), X(wr_sectors), X(wr_ticks), \
+  X(in_flight), X(io_ticks), X(time_in_queue)
 
 static void collect_block_dev(struct stats_type *type, const char *dev)
 {
-  char path[80];
-  FILE *file = NULL;
-
-  snprintf(path, sizeof(path), "/sys/block/%s/stat", dev);
-  file = fopen(path, "r");
-  if (file == NULL) {
-    ERROR("cannot open `%s': %m\n", path);
-    goto out;
-  }
-
-#define BLOCK_KEYS \
-  X(rd_reqs), X(rd_merges), X(rd_sectors), X(rd_ms), \
-  X(wr_reqs), X(wr_merges), X(wr_sectors), X(wr_ms), \
-  X(cur_reqs), X(io_ms), X(io_avg_ms)
-
-#define X(K) K
-  unsigned long long BLOCK_KEYS;
-#undef X
-
-#define X(K) &K
-  if (fscanf(file, "%llu %llu %llu %llu %llu %llu %llu %llu %llu %llu %llu",
-             BLOCK_KEYS) != 11)
-    goto out;
-#undef X
-
   struct stats *stats = get_current_stats(type, dev);
   if (stats == NULL)
-    goto out;
+    return;
 
-#define X(K) stats_set(stats, #K, K)
-  BLOCK_KEYS;
+  char path[80];
+  snprintf(path, sizeof(path), "/sys/block/%s/stat", dev);
+
+#define X(K) #K
+  collect_key_list(stats, path, BLOCK_KEYS, NULL);
 #undef X
-
- out:
-  if (file != NULL)
-    fclose(file);
 }
 
 static void collect_block(struct stats_type *type)
@@ -83,9 +59,7 @@ static void collect_block(struct stats_type *type)
 struct stats_type ST_BLOCK_TYPE = {
   .st_name = "block",
   .st_collect = &collect_block,
-  .st_schema = (char *[]) {
 #define X(K) #K
-    BLOCK_KEYS, NULL,
+  .st_schema = (char *[]) { BLOCK_KEYS, NULL, },
 #undef X
-  },
 };
