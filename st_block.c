a43df5227a6cba9d0c7573884a12684da61006f0
John L. Hammond <jhammond@ices.utexas.edu>
2011-03-22 09:41:11 -0500

    Add doc to ST_BLOCK.

--- st_block.c
+++ st_block.c
@@ -6,14 +6,22 @@
 #include "trace.h"
 
 /* Need to account for units.  According to block/stat.txt, in
-/sys/block/DEV/stat sector means 512B (as opposed to real sector size
-of device). */
+   /sys/block/DEV/stat sector means 512B (as opposed to real sector
+   size of device). */
 /* All X_ticks members and time_in_queue are in ms. */
 
 #define BLOCK_KEYS \
-  X(rd_ios), X(rd_merges), X(rd_sectors), X(rd_ticks), \
-  X(wr_ios), X(wr_merges), X(wr_sectors), X(wr_ticks), \
-  X(in_flight), X(io_ticks), X(time_in_queue)
+  X(rd_ios, "event", "read requests processed"), \
+  X(rd_merges, "event", "read requests merged with in-queue requests"), \
+  X(rd_sectors, "event,unit=512B", "sectors read"), \
+  X(rd_ticks, "event,unit=ms", "wait time for read requests"), \
+  X(wr_ios, "event", "write requests processed"), \
+  X(wr_merges, "event", "write requests merged with in-queue requests"), \
+  X(wr_sectors, "event,unit=512B", "sectors written"), \
+  X(wr_ticks, "event,unit=ms", "wait time for write requests"), \
+  X(in_flight, "requests in flight"), \
+  X(io_ticks, "event,unit=ms", "time active"), \
+  X(time_in_queue, "event,unit=ms", "wait time for all requests")
 
 static void collect_block_dev(struct stats_type *type, const char *dev)
 {
