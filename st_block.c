c9676da4c80f0e238880bca49b9cc87356f2106f
John L. Hammond <jhammond@ices.utexas.edu>
2011-03-23 12:20:17 -0500

    Niceify BLOCK_KEYS.

--- st_block.c
+++ st_block.c
@@ -11,17 +11,17 @@
 /* All X_ticks members and time_in_queue are in ms. */
 
 #define BLOCK_KEYS \
-  X(rd_ios, "event", "read requests processed"), \
-  X(rd_merges, "event", "read requests merged with in-queue requests"), \
-  X(rd_sectors, "event,unit=512B", "sectors read"), \
-  X(rd_ticks, "event,unit=ms", "wait time for read requests"), \
-  X(wr_ios, "event", "write requests processed"), \
-  X(wr_merges, "event", "write requests merged with in-queue requests"), \
-  X(wr_sectors, "event,unit=512B", "sectors written"), \
-  X(wr_ticks, "event,unit=ms", "wait time for write requests"), \
-  X(in_flight, "requests in flight"), \
-  X(io_ticks, "event,unit=ms", "time active"), \
-  X(time_in_queue, "event,unit=ms", "wait time for all requests")
+  X(rd_ios,        "event",           "read requests processed"), \
+  X(rd_merges,     "event",           "read requests merged with in-queue requests"), \
+  X(rd_sectors,    "event,unit=512B", "sectors read"), \
+  X(rd_ticks,      "event,unit=ms",   "wait time for read requests"), \
+  X(wr_ios,        "event",           "write requests processed"), \
+  X(wr_merges,     "event",           "write requests merged with in-queue requests"), \
+  X(wr_sectors,    "event,unit=512B", "sectors written"), \
+  X(wr_ticks,      "event,unit=ms",   "wait time for write requests"), \
+  X(in_flight,     "",                "requests in flight"), \
+  X(io_ticks,      "event,unit=ms",   "time active"), \
+  X(time_in_queue, "event,unit=ms",   "wait time for all requests")
 
 static void collect_block_dev(struct stats_type *type, const char *dev)
 {
