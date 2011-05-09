dc50bf965f750607e360c584c017578a6f429ce1
John L. Hammond <jhammond@ices.utexas.edu>
2011-05-09 12:27:46 -0500

    Rewrite llite.c to use sb_dict.

--- llite.c
+++ llite.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <fcntl.h>
 #include <dirent.h>
+#include <mntent.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
 #include "stats.h"
 #include "trace.h"
+#include "dict.h"
+#define OBD_IOC_GETNAME 0xC0086683
+#define LLITE_DIR_PATH "/proc/fs/lustre/llite"
 
 /* Based on llite_opcode_table in lustre-1.8.5/lustre/llite/lproc_llite.c.
 
@@ -20,7 +27,7 @@
    If you find out what 'regs' are then please let me know.
 */
 
-#define SCHEMA \
+#define KEYS \
   X(read_bytes, "E,U=B", ""), \
   X(write_bytes, "E,U=B", ""), \
   X(direct_read, "E,U=B", ""), \
@@ -45,7 +52,86 @@
   X(removexattr, "E", ""), \
   X(inode_permission, "E", "")
 
-#define LLITE_DIR_PATH "/proc/fs/lustre/llite"
+static int sb_dict_init(struct dict *sb_dict)
+{
+  int rc = -1;
+  const char *me_path = "/proc/mounts";
+  FILE *me_file = NULL;
+
+  if (dict_init(sb_dict, 8) < 0) {
+    ERROR("cannot create sb_dict: %m\n");
+    goto out;
+  }
+
+  me_file = setmntent(me_path, "r");
+  if (me_file == NULL) {
+    ERROR("cannot open `%s': %m\n", me_path);
+    goto out;
+  }
+
+  struct mntent me;
+  char me_buf[4096];
+  while (getmntent_r(me_file, &me, me_buf, sizeof(me_buf)) != NULL) {
+    int me_dir_fd = -1;
+    char me_lov_uuid[40];
+
+    if (strcmp(me.mnt_type, "lustre") != 0)
+      goto next;
+
+    me_dir_fd = open(me.mnt_dir, O_RDONLY);
+    if (me_dir_fd < 0) {
+      ERROR("cannot open `%s': %m\n", me.mnt_dir);
+      goto next;
+    }
+
+    if (ioctl(me_dir_fd, OBD_IOC_GETNAME, me_lov_uuid) < 0) {
+      ERROR("cannot get lov uuid for `%s': %m\n", me.mnt_dir);
+      goto next;
+    }
+
+    /* mnt_lov_uuid is of the form `work-clilov-ffff8102658ec800'. */
+    if (strlen(me_lov_uuid) < 24) {
+      ERROR("unrecognized lov uuid `%s'\n", me_lov_uuid);
+      goto next;
+    }
+
+    /* Get superblock address (the `ffff8102658ec800' part). */
+    const char *sb = me_lov_uuid + strlen(me_lov_uuid) - 16;
+    hash_t sb_hash = dict_strhash(sb);
+    struct dict_entry *sb_ent = dict_entry_ref(sb_dict, sb_hash, sb);
+    if (sb_ent->d_key != NULL) {
+      TRACE("multiple filesystems with sb `%s'\n", sb);
+      goto next;
+    }
+
+    char *sb_key = malloc(strlen(sb) + 1 + strlen(me.mnt_dir) + 1);
+    if (sb_key == NULL) {
+      ERROR("cannot allocate sb_key: %m\n");
+      goto next;
+    }
+
+    strcpy(sb_key, sb);
+    strcpy(sb_key + strlen(sb) + 1, me.mnt_dir);
+
+    if (dict_entry_set(sb_dict, sb_ent, sb_hash, sb_key) < 0) {
+      ERROR("cannot set sb_dict entry: %m\n");
+      free(sb_key);
+      goto next;
+    }
+
+    TRACE("fsname `%s', dir `%s', type `%s', lov_uuid `%s', sb `%s'\n",
+          me.mnt_fsname, me.mnt_dir, me.mnt_type, me_lov_uuid, sb);
+
+  next:
+    if (me_dir_fd >= 0)
+      close(me_dir_fd);
+  }
+  rc = 0;
+ out:
+  if (me_file != NULL)
+    endmntent(me_file);
+  return rc;
+}
 
 static void collect_llite_fs(struct stats *stats, const char *d_name)
 {
@@ -100,53 +186,70 @@ static void collect_llite_fs(struct stats *stats, const char *d_name)
 
 static void collect_llite(struct stats_type *type)
 {
-  const char *dir_path = LLITE_DIR_PATH;
-  DIR *dir = NULL;
+  const char *llite_dir_path = LLITE_DIR_PATH;
+  DIR *llite_dir = NULL;
+  DEFINE_DICT(sb_dict);
 
-  dir = opendir(dir_path);
-  if (dir == NULL) {
-    ERROR("cannot open `%s': %m\n", dir_path);
+  if (sb_dict_init(&sb_dict) < 0) {
+    ERROR("cannot create sb_dict: %m\n");
+    goto out;
+  }
+
+  TRACE("found %zu lustre filesystems\n", sb_dict.d_count);
+  if (sb_dict.d_count == 0)
+    goto out;
+
+#ifdef DEBUG
+  do {
+    size_t i = 0;
+    char *sb;
+    while ((sb = dict_for_each(&sb_dict, &i)) != NULL)
+      TRACE("sb `%s', dir `%s'\n", sb, sb + strlen(sb) + 1);
+  } while (0);
+#endif
+
+  llite_dir = opendir(llite_dir_path);
+  if (llite_dir == NULL) {
+    ERROR("cannot open `%s': %m\n", llite_dir_path);
     goto out;
   }
 
   struct dirent *ent;
-  while ((ent = readdir(dir)) != NULL) {
-    char *dev = NULL;
+  while ((ent = readdir(llite_dir)) != NULL) {
     struct stats *stats = NULL;
+    char *name = ent->d_name, *sb, *sb_ref, *mnt;
 
-    if (*ent->d_name == '.')
-      goto next;
+    if (*name == '.' || strlen(name) < 16)
+      continue;
 
-    /* Entries in /proc/fs/lustre/llite look like scratch-ffff81019a4eb000. */
-    /* We want to chop off the hex part. */
+    sb = name + strlen(name) - 16;
+    sb_ref = dict_ref(&sb_dict, sb);
+    if (sb_ref == NULL) {
+      ERROR("no superblock found for llite entry `%s'\n", name);
+      continue;
+    }
 
-    dev = strdup(ent->d_name);
-    if (dev == NULL)
-      goto next; /* ... */
+    mnt = sb_ref + strlen(sb_ref) + 1;
+    TRACE("ent `%s', sb_ref `%s', mnt `%s'\n", name, sb_ref, mnt);
 
-    char *p_dash = strrchr(dev, '-');
-    if (p_dash != NULL)
-      *p_dash = 0;
-
-    stats = get_current_stats(type, dev);
+    stats = get_current_stats(type, mnt);
     if (stats == NULL)
-      goto next;
+      continue;
 
-    collect_llite_fs(stats, ent->d_name);
-
-  next:
-    free(dev);
+    collect_llite_fs(stats, name);
   }
 
  out:
-  if (dir != NULL)
-    closedir(dir);
+  dict_destroy(&sb_dict, &free);
+
+  if (llite_dir != NULL)
+    closedir(llite_dir);
 }
 
 struct stats_type STATS_TYPE_LLITE = {
   .st_name = "llite",
   .st_collect = &collect_llite,
 #define X(k,o,d,r...) #k "," o
-  .st_schema_def = JOIN(SCHEMA),
+  .st_schema_def = JOIN(KEYS),
 #undef X
 };
