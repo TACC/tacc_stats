fa6c78201044e7be983483841fee7189f5a6729c
John L. Hammond <jhammond@ices.utexas.edu>
2011-06-20 14:46:24 -0500

    Move amd64 handling into job_stats.py.

--- job_stats.py
+++ job_stats.py
@@ -6,14 +6,15 @@ import gzip, os, numpy, signal, string, subprocess, sys, time
 # TODO Sanity check on rollover.
 # TODO Check that input values are not wider than allowed.
 
-opt_verbose = True # XXX
+verbose = True # XXX
 prog = os.path.basename(sys.argv[0])
 if prog == "":
     prog = "***"
 
 def trace(fmt, *args):
-    msg = fmt % args
-    sys.stderr.write(prog + ": " + msg)
+    if verbose:
+        msg = fmt % args
+        sys.stderr.write(prog + ": " + msg)
 
 def error(fmt, *args):
     msg = fmt % args
@@ -59,6 +60,48 @@ def get_job_info(id):
     return info
 
 
+# amd64
+# define PERF_EVENT(event_select, unit_mask) \
+#  ( (event_select & 0xFF) \
+#  | (unit_mask << 8) \
+#  | (1UL << 16) /* Count in user mode (CPL == 0). */ \
+#  | (1UL << 17) /* Count in OS mode (CPL > 0). */ \
+#  | (1UL << 22) /* Enable. */ \
+#  | ((event_select & 0xF00) << 24) \
+#  )
+
+def amd64_perf_event(event_select, unit_mask):
+    return (event_select & 0xFF) | (unit_mask << 8) | (1L << 16) | (1L << 17) | (1L << 22) | ((event_select & 0xF00) << 24)
+
+#define DRAMaccesses   PERF_EVENT(0xE0, 0x07) /* DCT0 only */
+#define HTlink0Use     PERF_EVENT(0xF6, 0x37) /* Counts all except NOPs */
+#define HTlink1Use     PERF_EVENT(0xF7, 0x37) /* Counts all except NOPs */
+#define HTlink2Use     PERF_EVENT(0xF8, 0x37) /* Counts all except NOPs */
+#define UserCycles    (PERF_EVENT(0x76, 0x00) & ~(1UL << 17))
+#define DCacheSysFills PERF_EVENT(0x42, 0x01) /* Counts DCache fills from beyond the L2 cache. */
+#define SSEFLOPS       PERF_EVENT(0x03, 0x7F) /* Counts single & double, add, multiply, divide & sqrt FLOPs. */
+
+dram_accesses = amd64_perf_event(0xE0, 0x07)
+ht_link_0_use = amd64_perf_event(0xF6, 0x37)
+ht_link_1_use = amd64_perf_event(0xF7, 0x37)
+ht_link_2_use = amd64_perf_event(0xF8, 0x37)
+user_cycles = amd64_perf_event(0x76, 0x00) & ~(1L << 17)
+dcache_sys_fills = amd64_perf_event(0x42, 0x01)
+sse_flops = amd64_perf_event(0x03, 0x7F)
+
+amd64_core_ctls = {
+    user_cycles: 0,
+    dcache_sys_fills: 1,
+    sse_flops: 2,
+    }
+
+amd64_sock_ctls = {
+    dram_accesses: 0,
+    ht_link_0_use: 1,
+    ht_link_1_use: 2,
+    ht_link_2_use: 3,
+    }
+
 class Job(object):
     def __init__(self, id, info=None):
         self.id = str(id)
@@ -88,17 +131,65 @@ class Job(object):
                 for dev in type_data.devs():
                     self.types[type_name].devs.add(dev)
         # TODO Warn about bad hosts.
-    def get_schema(self, type_name, schema_desc):
-        # TODO Warn about schema changes.
-        data = self.types.get(type_name)
-        if not data:
-            data = JobTypeData(type_name)
-            self.types[type_name] = data
-        schema = data.schemas.get(schema_desc)
-        if not schema:
-            schema = Schema(type_name, schema_desc)
-            data.schemas[schema_desc] = schema
-        return schema
+        if 'amd64_pmc' in self.types:
+            self.process_amd64()
+    def process_amd64(self):
+        for host_entry in self.hosts.itervalues():
+            orig_data = host_entry.types['amd64_pmc']
+            core_data = host_entry.add_type('amd64_core', 'USER,E DCSF,E SSE_FLOPS,E')
+            sock_data = host_entry.add_type('amd64_sock', 'DRAM,E HT0,E HT1,E HT2,E')
+            # Assume no stray times.
+            times = orig_data.times['0']
+            nr_rows = len(times)
+            for sock_nr in range(0, 4):
+                sock_dev = str(sock_nr)
+                sock_data.times[sock_dev] = numpy.array(times, numpy.uint64)
+                sock_stats = sock_data.stats[sock_dev] = numpy.zeros((nr_rows, 4), numpy.uint64)
+                for core_nr in range(4 * sock_nr, 4 * (sock_nr + 1)):
+                    core_dev = str(core_nr)
+                    orig_stats = orig_data.stats[core_dev]
+                    core_data.times[core_dev] = numpy.array(times, numpy.uint64)
+                    core_stats = core_data.stats[core_dev] = numpy.zeros((nr_rows, 3), numpy.uint64)
+                    # Assume schema is still CTL{0..3} CTR{0..3}.
+                    for row in range(0, nr_rows):
+                        for ctl_nr in range(0, 4):
+                            ctl = orig_stats[row][ctl_nr]
+                            val = orig_stats[row][ctl_nr + 4]
+                            if ctl in amd64_sock_ctls:
+                                col = amd64_sock_ctls[ctl]
+                                if col == 0: # This is horrible.
+                                    val *= 64 # DRAM accesses are 64B.
+                                else:
+                                    val *= 4 # Each HT event counter increment represents 4B.
+                                sock_stats[row][col] += val
+                            elif ctl in amd64_core_ctls:
+                                core_stats[row][amd64_core_ctls[ctl]] += val
+                            else:
+                                # TODO Improve error detection.
+                                error("unknown PMC control value %d\n", ctl)
+                                del self.types['amd64_core']
+                                del self.types['amd64_sock']
+                                return
+        self.types['amd64_core'].devs = set(map(str, range(0, 16)))
+        self.types['amd64_sock'].devs = set(map(str, range(0, 4)))
+    def get_schema(self, type_name, desc=None):
+        if desc:
+            # TODO Warn about schema changes.
+            data = self.types.get(type_name)
+            if not data:
+                data = JobTypeData(type_name)
+                self.types[type_name] = data
+            schema = data.schemas.get(desc)
+            if not schema:
+                schema = Schema(type_name, desc)
+                data.schemas[desc] = schema
+            return schema
+        else:
+            if len(self.types[type_name].schemas) != 1:
+                # error
+                return None
+            for schema in self.types[type_name].schemas.itervalues():
+                return schema
 
 
 class JobTypeData(object):
@@ -181,7 +272,7 @@ class HostEntry(object):
     def add_type(self, type_name, schema_desc):
         type_data = self.types.get(type_name)
         if not type_data:
-            schema = self.job.get_schema(type_name, schema_desc)
+            schema = self.job.get_schema(type_name, desc=schema_desc)
             type_data = HostTypeData(type_name, schema)
             self.types[type_name] = type_data
         if type_data.schema.desc != schema_desc: # BLECH.
