35e2b83498e5fa0704569ceda0198a2f76bf21a6
John Hammond <jhammond@tacc.utexas.edu>
2011-11-08 11:01:41 -0600

    Rename collect_cpu() and use str_collect_key_list().

--- cpu.c
+++ cpu.c
@@ -5,53 +5,25 @@
 #include <malloc.h>
 #include <ctype.h>
 #include "stats.h"
+#include "collect.h"
 #include "trace.h"
 #include "string1.h"
 
 /* The /proc manpage says units are units of 1/sysconf(_SC_CLK_TCK)
-   second.  sysconf(_SC_CLK_TCK) seems to always be 100. */
+   seconds.  sysconf(_SC_CLK_TCK) seems to always be 100. */
 
 /* We ignore steal and guest. */
 
 #define KEYS \
-  X(user, "E,U=cs", "time in user mode"), \
-  X(nice, "E,U=cs", "time in user mode with low priority"), \
-  X(system, "E,U=cs", "time in system mode"), \
-  X(idle, "E,U=cs", "time in idle task"), \
-  X(iowait, "E,U=cs", "time in I/O wait"), \
-  X(irq, "E,U=cs", "time in IRQ"), \
+  X(user,    "E,U=cs", "time in user mode"), \
+  X(nice,    "E,U=cs", "time in user mode with low priority"), \
+  X(system,  "E,U=cs", "time in system mode"), \
+  X(idle,    "E,U=cs", "time in idle task"), \
+  X(iowait,  "E,U=cs", "time in I/O wait"), \
+  X(irq,     "E,U=cs", "time in IRQ"), \
   X(softirq, "E,U=cs", "time in softIRQ")
 
-static void collect_proc_stat_cpu(struct stats_type *type, char *cpu, char *rest)
-{
-  /* Ignore the totals line and anything not matching [0-9]+. */
-  char *s = cpu;
-
-  if (*s == 0)
-    return;
-
-  for (; *s != 0; s++)
-    if (!isdigit(*s))
-      return;
-
-  struct stats *cpu_stats = get_current_stats(type, cpu);
-  if (cpu_stats == NULL)
-    return;
-
-#define X(k,r...) k = 0
-  unsigned long long KEYS;
-#undef X
-
-#define X(k,r...) &k
-  sscanf(rest, "%llu %llu %llu %llu %llu %llu %llu", KEYS);
-#undef X
-
-#define X(k,r...) stats_set(cpu_stats, #k, k)
-  KEYS;
-#undef X
-}
-
-static void collect_proc_stat(struct stats_type *type)
+static void cpu_collect(struct stats_type *type)
 {
   const char *path = "/proc/stat";
   FILE *file = NULL;
@@ -67,15 +39,26 @@ static void collect_proc_stat(struct stats_type *type)
   setvbuf(file, file_buf, _IOFBF, sizeof(file_buf));
 
   while (getline(&line, &line_size, file) >= 0) {
-    char *key, *rest = line;
-    key = wsep(&rest);
-    if (key == NULL || rest == NULL)
+    char *rest = line;
+    char *cpu = wsep(&rest);
+    if (cpu == NULL || rest == NULL)
       continue;
 
-    if (strncmp(key, "cpu", 3) != 0)
+    if (strncmp(cpu, "cpu", 3) != 0)
       continue;
 
-    collect_proc_stat_cpu(type, key + 3, rest);
+    cpu += 3;
+
+    if (!isdigit(*cpu))
+      continue;
+
+    struct stats *stats = get_current_stats(type, cpu);
+    if (stats == NULL)
+      continue;
+
+#define X(k,r...) #k
+    str_collect_key_list(rest, stats, KEYS, NULL);
+#undef X
   }
 
  out:
@@ -86,7 +69,7 @@ static void collect_proc_stat(struct stats_type *type)
 
 struct stats_type cpu_stats_type = {
   .st_name = "cpu",
-  .st_collect = &collect_proc_stat,
+  .st_collect = &cpu_collect,
 #define X SCHEMA_DEF
   .st_schema_def = JOIN(KEYS),
 #undef X
