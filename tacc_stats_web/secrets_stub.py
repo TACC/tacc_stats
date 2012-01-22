45ec0bd53462ac6ce5b80dafb0ed32f4877ae000
Andy R. Terrel <andy.terrel@gmail.com>
2012-01-22 14:48:25 -0600

    Adding secrets_stub

---
+++ tacc_stats_web/secrets_stub.py
@@ -0,0 +1,14 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'db',                      # Or path to database file if using sqlite3.
+        'USER': 'db_user',                      # Not used with sqlite3.
+        'PASSWORD': 'db_user_pass',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = '<some_random_chars_make_it_nice_and_long>'
