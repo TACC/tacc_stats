81cc3ec24f0ce60cd6f753e6493354884587ca11
Bill Barth <bbarth@tacc.utexas.edu>
2012-09-28 15:05:30 -0500

    added plot of user cycles

--- analyze/process_pickles/masterplot.py
+++ analyze/process_pickles/masterplot.py
@@ -18,8 +18,10 @@ def main():
 
   filelist=tspl.getfilelist(n.filearg)
 
-  k1=['amd64_core','amd64_core','amd64_sock','lnet','lnet','ib_sw','ib_sw']
-  k2=['SSE_FLOPS','DCSF','DRAM','rx_bytes','tx_bytes','rx_bytes','tx_bytes']
+  k1=['amd64_core','amd64_core','amd64_sock','lnet','lnet','ib_sw','ib_sw',
+      'cpu']
+  k2=['SSE_FLOPS','DCSF','DRAM','rx_bytes','tx_bytes','rx_bytes','tx_bytes',
+      'user']
 
   for file in filelist:
     try:
@@ -38,7 +40,7 @@ def main():
     tmid=(ts.t[:-1]+ts.t[1:])/2.0
     
 
-    fig,ax=plt.subplots(5,1,figsize=(8,12),dpi=80)
+    fig,ax=plt.subplots(6,1,figsize=(8,12),dpi=80)
     
     # Plot flop rate
     ax[0].hold=True 
@@ -80,9 +82,18 @@ def main():
       v=ts.data[5][k]+ts.data[6][k]-(ts.data[3][k]+ts.data[4][k])
       rate=numpy.divide(numpy.diff(v),numpy.diff(ts.t))
       ax[4].plot(tmid/3600,rate/(1024*1024.))
-    ax[4].set_xlabel('Time (hr)')
     ax[4].set_ylabel('Total (ib_sw-lnet) MB/s')
 
+    #Plot CPU user time
+    ax[5].hold=True
+    for k in ts.j.hosts.keys():
+      h=ts.j.hosts[k]
+      rate=numpy.divide(numpy.diff(ts.data[7][k]/100/ts.wayness),
+                        numpy.diff(ts.t))
+      ax[5].plot(tmid/3600,rate)
+    ax[5].set_ylabel('Total ' + ts.k1[7] + '\n' + ts.k2[7] + '/s')
+    ax[4].set_xlabel('Time (hr)')
+    
     print ts.j.id + ': '
 
     plt.suptitle(ts.title)
