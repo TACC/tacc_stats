621d6e978f4f2c51e0d458f26e0e1dc2f6fe1d16
Kyle Marcus <kmarcus2@buffalo.edu>
2013-06-14 14:46:24 -0400

    added support to parse torque accounting files in reader function

--- monitor/torque_acct.py
+++ monitor/torque_acct.py
@@ -1,4 +1,4 @@
-import csv, os, subprocess
+import csv, os, subprocess, datetime
 
 # TORQUE/PBS accounting file
 # docs.adaptivecomputing.com/torque/4-1-3/help.htm#topics/9-accounting/accountingRecords.htm
@@ -25,53 +25,100 @@ import csv, os, subprocess
 # qtime            Time job was queued
 # start            Time job started to run
 
-# NOTES
-#  - how to figure out end time? current_time - start_time if status == E
-
 
 stats_home = os.getenv('TACC_STATS_HOME', '/scratch/projects/tacc_stats')
 acct_path = os.getenv('TACC_STATS_ACCT', os.path.join(stats_home, 'accounting'))
 
 fields = (
-    ('current_date'     str, 'Current date'),
-    ('current_time'     str, 'Current time'),
-    ('status'           str, 'Job status'),
-    ('id'               str, 'Job identifier'),
-    ('hostname'         str, 'Name of the execution host'),
-    ('user'             str, 'User that is running the job'),
-    ('group'            str, 'Group id of the job owner'),
-    ('name'             str, 'Job name'),
-    ('queue'            str, 'Queue name the job is running on'),
-    ('create_time'      int, 'Time job was created (unix time stamp)'),
-    ('queue_time'       int, 'Time job was queued (unix time stamp)'),
-    ('eligible_time')   int, 'Time job was eligible to run (unix time stamp)'),
-    ('start_time')      int, 'Time job started to run (unix time stamp)'),
-    ('owner'            str, 'Owner of the job at hostname'),
-    ('nodes'            str, 'Nodes assigned to job'),
-    ('walltime')        str, 'Walltime requested')
+    ('id'                          int, 'Job identifier'),
+    ('hostname'                    str, 'Hostname of the head execution node'),
+    ('user'                        str, 'User that is running the job'),
+    ('group'                       str, 'Group name of the job owner'),
+    ('jobname'                     str, 'Job name'),
+    ('queue'                       str, 'Queue name the job is running on'),
+    ('ctime'                       int, 'Time job was created (unix time stamp)'),
+    ('qtime'                       int, 'Time job was queued (unix time stamp)'),
+    ('etime'                       int, 'Time job was eligible to run (unix time stamp)'),
+    ('start'                       int, 'Time job started to run (unix time stamp)'),
+    ('owner'                       str, 'Owner of the job at hostname'),
+    ('exec_host'                   str, 'List of nodes used'),
+    ('Resource_List.neednodes'     str, 'Requested nodes needed'),
+    ('Resource_List.nodect'        str, 'Requested number of nodes'),
+    ('Resource_List.nodes'         str, 'Requested nodes'),
+    ('Resource_List.walltime'      str, 'Requested walltime'),
+    ('session'                     int, 'Session id'),
+    ('end'                         int, 'Time job ended (unix time stamp)'),
+    ('Exit_status'                 int, 'Exit status of job'),
+    ('resources_used.cput'         str, 'CPU time used'),
+    ('resources_used.mem'          str, 'Memory used'),
+    ('resources_used.vmem'         str, 'Virtual memory used'),
+    ('resources_used.walltime'     str, 'Walltime'),
 )
 
-field_names = [tup[0] for tup in fields]
 
+# reader(dir, start_time=0, end_time=9223372036854775807L):
+#  Info:
+#   Reads in TORQUE accounting files and searches for jobs that have ended
+#   between the specified dates.
+#  Inputs:
+#   dir - directory where torque accounting files are located
+#   start_time - look for jobs starting at this date
+#   end_time - look for jobs ending at this time
+#  Returns:
+#   Iterator for all jobs that finished between start_time and end_time, the
+#   iterator is returned with the yield command so it will use less memory.
+def reader(dir, start_time=0, end_time=9223372036854775807L):
 
-def reader(file, start_time=0, end_time=9223372036854775807L, seek=0):
-    """reader(file, start_time=0, end_time=9223372036854775807L, seek=0)
-    Return an iterator for all jobs that finished between start_time and end_time.
-    """
-    if type(file) == str:
-        file = open(file)
-    if seek:
-        file.seek(seek, os.SEEK_SET)
-    for d in csv.DictReader(file, delimiter=':', fieldnames=field_names):
-        try:
-            for n, t, x in fields:
-                d[n] = t(d[n])
-        except:
-            pass
-        # Accounting records with pe_taskid != NONE are generated for
-        # sub_tasks of a tightly integrated job and should be ignored.
-        if start_time <= d['end_time'] and d['end_time'] < end_time and d['pe_taskid'] == 'NONE':
-            yield d
+    # check if a directory is given, accounting files for TORQUE are stored in
+    # a directory named by the date YYYYMMDD
+    if not os.path.isdir(dir)
+        return None
+
+    # turn unix timestamp into date object
+    start_date = datetime.date.fromtimestamp(start_time)
+    end_date = datetime.date.fromtimestamp(end_time)
+
+    # loop through all dates specified
+    while ( start_date <= end_date ):
+
+        # open accounting file
+        acct_record_file = open( os.path.join( dir, start_date.strftime('%Y%m%d') ), 'r')
+
+        # loop through records in accounting file
+        for acct_record in acct_record_file:
+
+            # check if record corresponds to a job ending
+            acct_data = acct_record.split(';')
+            if acct_data[1] == 'E':
+
+                # put record into associative array
+                acct_data_arr = {}
+                acct_data_arr['id'] = acct_data[2][:acct_data[2].find('.')]
+                acct_data = acct_data[3].split(' ')
+
+                for stat in acct_data:
+                    acct_data_arr[ stat[:stat.find('=')] ] = stat[stat.find('=')+1:]
+
+                acct_data_arr['hostname'] = acct_data_arr['exec_host'][:acct_data_arr['exec_host'].find('/')]
+
+                # convert data types in array
+                try:
+                    for stat_name, stat_type, stat_desc in fields:
+                        acct_data_arr[stat_name] = stat_type(acct_data_arr[stat_name])
+                except:
+                    pass
+
+                # change key names
+                acct_data_arr['start_time'] = acct_data_arr['start']
+                del acct_data_arr['start']
+                acct_data_arr['end_time'] = acct_data_arr['end']
+                del acct_data_arr['end']
+
+                # check start and end times then yield job array (yield returns a itterator)
+                if start_time <= acct_data_arr['end_time'] and acct_data_arr['end_time'] < end_time:
+                    yield acct_data_arr
+
+        start_date += datetime.timedelta(days=1)
 
 
 def from_id_with_file_1(id, acct_file):
