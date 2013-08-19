35b70684fe48e99fecb22956700d275ec642ef4d
Richard Evans <rtevans@staff.stampede.tacc.utexas.edu>
2013-08-19 15:05:19 -0500

    added function to get cpuid signatures from intel chips.

--- monitor/intel_pmc3.c
+++ monitor/intel_pmc3.c
@@ -74,12 +74,26 @@
   X(PERF_GLOBAL_CTRL, "C", ""), \
   X(PERF_GLOBAL_OVF_CTRL, "C", "")
 
+static void get_cpuid_signature(int cpuid_file, char* signature)
+{
+  int ebx = 0, ecx = 0, edx = 0, eax = 1;
+  __asm__ ("cpuid": "=b" (ebx), "=c" (ecx), "=d" (edx), "=a" (eax):"a" (eax));
+
+  int model = (eax & 0x0FF) >> 4;
+  int extended_model = (eax & 0xF0000) >> 12;
+  int family_code = (eax & 0xF00) >> 8;
+  int extended_family_code = (eax & 0xFF00000) >> 16;
+
+  snprintf(signature,sizeof(signature),"%02x_%x", extended_family_code | family_code, extended_model | model);
+
+}
 static int cpu_is_nehalem(char *cpu)
 {
   char cpuid_path[80];
   int cpuid_fd = -1;
   uint32_t buf[4];
   int rc = 0;
+  char signature[5];
 
   /* Open /dev/cpuid/cpu/cpuid. */
   snprintf(cpuid_path, sizeof(cpuid_path), "/dev/cpu/%s/cpuid", cpu);
@@ -88,7 +102,7 @@ static int cpu_is_nehalem(char *cpu)
     ERROR("cannot open `%s': %m\n", cpuid_path);
     goto out;
   }
-
+  
   /* Get cpu vendor. */
   if (pread(cpuid_fd, buf, sizeof(buf), 0x0) < 0) {
     ERROR("cannot read cpu vendor through `%s': %m\n", cpuid_path);
@@ -105,6 +119,9 @@ static int cpu_is_nehalem(char *cpu)
     ERROR("cannot read `%s': %m\n", cpuid_path);
     goto out;
   }
+
+  get_cpuid_signature(cpuid_fd,signature);
+  TRACE("cpu%s, CPUID Signature %s\n",cpu,signature);
 
   TRACE("cpu %s, buf %08x %08x %08x %08x\n", cpu, buf[0], buf[1], buf[2], buf[3]);
 
