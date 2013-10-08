106399cb58e4cc1cdb27778f6306a3ddbbde5385
Bill Barth <bbarth@tacc.utexas.edu>
2013-10-08 16:27:24 -0500

    added some metadata rate plot

--- analyze/process_pickles/plot.py
+++ analyze/process_pickles/plot.py
@@ -43,9 +43,24 @@ def main():
   for file in filelist:
     try:
       ts=tspl.TSPLSum(file,['intel_snb_imc', 'intel_snb_imc', 'intel_snb',
-                            'intel_snb', 'intel_snb', 'intel_snb'],
+                            'intel_snb', 'intel_snb', 'intel_snb','llite',
+                            'llite', 'llite', 'llite', 'llite', 'llite',
+                            'llite', 'llite', 'llite', 'llite', 'llite',
+                            'llite', 'llite', 'llite', 'llite', 'llite',
+                            'llite', 'llite', 'llite', 'llite', 'llite',
+                            'llite', 'llite', 'llite', 'llite', 'llite',],
                       ['CAS_READS', 'CAS_WRITES', 'STALLS',
-                      'CLOCKS_UNHALTED_CORE', 'SSE_D_ALL', 'SIMD_D_256'])
+                       'CLOCKS_UNHALTED_CORE', 'SSE_D_ALL', 'SIMD_D_256',
+                       'open','close','mmap','seek','fsync','setattr',
+                       'truncate','flock','getattr','statfs','alloc_inode',
+                       'setxattr','getxattr',' listxattr',
+                       'removexattr', 'inode_permission', 'readdir',
+                       'create','lookup',
+                       'link','unlink','symlink','mkdir','rmdir','mknod','rename',])
+      
+      
+      
+      
       
     except tspl.TSPLException as e:
       continue
@@ -57,15 +72,13 @@ def main():
 
     ld=lariat_utils.LariatData(ts.j.id,ts.j.end_time,'lariatData')
     
-    fig,ax=plt.subplots(3,1,figsize=(8,8),dpi=80)
-    plt.subplots_adjust(hspace=0.35)
-    
     read_rate=numpy.zeros_like(tmid)
     write_rate=numpy.zeros_like(tmid)
     stall_rate=numpy.zeros_like(tmid)
     clock_rate=numpy.zeros_like(tmid)
     avx_rate = numpy.zeros_like(tmid)
     sse_rate = numpy.zeros_like(tmid)
+    meta_rate = numpy.zeros_like(tmid)
 
     for k in ts.j.hosts.keys():
       read_rate +=numpy.diff(ts.assemble([0],k,0))/numpy.diff(ts.t)
@@ -74,7 +87,15 @@ def main():
       clock_rate+=numpy.diff(ts.assemble([3],k,0))/numpy.diff(ts.t)
       avx_rate  +=numpy.diff(ts.assemble([5],k,0))/numpy.diff(ts.t)
       sse_rate  +=numpy.diff(ts.assemble([4],k,0))/numpy.diff(ts.t)
+      meta_rate +=numpy.diff(ts.assemble(range(5,32),k,0))/numpy.diff(ts.t)
       
+    read_rate  /= float(ts.numhosts)
+    write_rate /= float(ts.numhosts)
+    stall_rate /= float(ts.numhosts)
+    clock_rate /= float(ts.numhosts)
+    avx_rate   /= float(ts.numhosts)
+    sse_rate   /= float(ts.numhosts)
+    meta_rate  /= float(ts.numhosts)
 
     read_frac=read_rate/(read_rate+write_rate+1)
     stall_frac=stall_rate/clock_rate
@@ -83,6 +104,9 @@ def main():
     if ld.exc != 'unknown':
       title += ', E: ' + ld.exc.split('/')[-1]
 
+    fig,ax=plt.subplots(4,1,figsize=(8,8),dpi=80)
+    plt.subplots_adjust(hspace=0.35)
+    
     plt.suptitle(title)
     ax[0].plot(tmid/3600., read_frac)
     ax[0].set_ylabel('DRAM Read Fraction')
@@ -93,17 +117,21 @@ def main():
 #    ax[1].set_ylabel('Stall Fraction')
 #    tspl_utils.adjust_yaxis_range(ax[1],0.1)
 
-    ax[1].plot(tmid/3600., avx_rate/1e9/float(ts.wayness*ts.numhosts))
+    ax[1].plot(tmid/3600., avx_rate/1e9)
     ax[1].hold=True
-    ax[1].plot(tmid/3600., sse_rate/1e9/float(ts.wayness*ts.numhosts),'r')
+    ax[1].plot(tmid/3600., sse_rate/1e9,'r')
     ax[1].set_ylabel('AVX Rate')
     tspl_utils.adjust_yaxis_range(ax[1],0.1)
 
 
-    ax[2].plot(tmid/3600., clock_rate/1.e9/float(ts.wayness*ts.numhosts))
+    ax[2].plot(tmid/3600., clock_rate)
     ax[2].set_ylabel('Observed Clock Rate')
     tspl_utils.adjust_yaxis_range(ax[2],0.1)
     
+    ax[3].plot(tmid/3600., meta_rate)
+    ax[3].set_ylabel('Meta Data Rate')
+    tspl_utils.adjust_yaxis_range(ax[3],0.1)
+
     fname='_'.join(['plot',ts.j.id,ts.owner])
 
     fig.savefig(fname)
