5159abd385f1ee1d8f734c48cb7d15b1b8e5170d
rtevans <rtevans@tacc-stats.tacc.utexas.edu>
2013-12-03 11:23:23 -0600

    fixing cron stuff for ls4

--- analyze/site/ls4_update_db.py
+++ analyze/site/ls4_update_db.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 from django.core.management import setup_environ
 import os,sys,subprocess
+from subprocess import Popen, PIPE
+
 sys.path.append(os.path.join(os.path.dirname(__file__), 
                              'tacc_stats_site'))
 sys.path.append(os.path.join(os.path.dirname(__file__), 
@@ -11,18 +13,29 @@ setup_environ(settings)
 import lonestar.views as views
 import lonestar.sys_path_append as sys_path_append
 import MetaData
-
+import datetime
 
 path = sys_path_append.pickles_dir
-date_str = sys.argv[1] 
+
+
+p = Popen(["date --date " + sys.argv[1] + ' +%Y-%m-%d'], stdout = PIPE, 
+                   shell = True) 
+date_start = p.communicate()[0].strip()
+
+p = Popen(["date --date " + sys.argv[2] + ' +%Y-%m-%d'], stdout = PIPE,
+                   shell = True) 
+date_end = p.communicate()[0].strip()
 
 for date in os.listdir(path):
+
+    s = [int(x) for x in date_start.split('-')]
+    e = [int(x) for x in date_end.split('-')]
+    d = [int(x) for x in date.split('-')]
+
+    if not datetime.date(s[0],s[1],s[2]) <= datetime.date(d[0],d[1],d[2]) <= datetime.date(e[0],e[1],e[2]): continue
     
-    date_str = subprocess.check_output(['date', '--date', date_str, '+%Y-%m-%d'])
-    #if date.strip() != date_str.strip(): continue
     print 'Run update for',date
     meta = MetaData.MetaData(os.path.join(path,date))
-    #if os.path.exists(meta.meta_path): continue
     meta.load_update()
     print 'Number of pickle files to upload into DB',len(meta.json.keys())
     views.ls4_update(meta = meta)
