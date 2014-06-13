81f5e966aa0002c235c9480d0cc021eea6ccdce8
Richard Evans <rtevans@staff.stampede.tacc.utexas.edu>
2014-06-13 10:41:11 -0500

    there was a bug in the pickler multiprocess that has been repaired

--- tacc_stats/pickler/job_pickles.py
+++ tacc_stats/pickler/job_pickles.py
@@ -24,30 +24,24 @@ def USAGE(str):
     print >>sys.stderr, "Usage: %s %s" % (__file__, str)
     sys.exit(1)
 
-"""
-def _pickle_method(method):
-    func_name = method.im_func.__name__
-    obj = method.im_self
-    cls = method.im_class
-    return _unpickle_method,(func_name,obj,cls)
-    
-def _unpickle_method(func_name,obj,cls):
-    for cls in cls.mro():
-        try:
-            func = cls.__dict__[func_name]
-        except KeyError:
-            pass 
-        else: break
-    return func.__get__(obj,cls)
+def job_pickle(reader_inst, 
+               pickle_dir = '.', 
+               tacc_stats_home = cfg.tacc_stats_home,
+               host_list_dir = cfg.host_list_dir,
+               acct = None,
+               pickle_prot = pickle.HIGHEST_PROTOCOL):
+    print(reader_inst)
+    if reader_inst['end_time'] == 0:
+        return
+    if os.path.exists(os.path.join(pickle_dir, reader_inst['id'])): 
+        print(reader_inst['id'] + " exists, don't reprocess")
+        return
 
-copy_reg.pickle(types.MethodType,_pickle_method,_unpickle_method)
-"""
-def unwrap(args):
-    print("runing>>>>>>>>>>>")
-    try:
-        args[0].job_pickle(args[1])
-    except:
-        print(traceback.format_exc())
+    job = job_stats.from_acct(reader_inst, 
+                              tacc_stats_home, 
+                              host_list_dir, acct)
+    with open(os.path.join(pickle_dir, job.id), 'wb') as pickle_file:
+        pickle.dump(job, pickle_file, pickle_prot)
 
 class JobPickles:
 
@@ -64,7 +58,6 @@ class JobPickles:
         self.end = kwargs.get('end',None)
         self.seek = kwargs.get('seek',0)
         self.acct = batch_acct.factory(self.batch_system, self.acct_path, self.host_name_ext)
-        #self.reader = self.acct.reader(start_time=self.getdate(self.start),end_time=self.getdate(self.end),seek=self.seek)
 
     def getdate(self,date_str):
         try:        
@@ -78,42 +71,35 @@ class JobPickles:
         except subprocess.CalledProcessError, e:
             FATAL("Invalid date: `%s'" % (date_str,))
 
-    def job_pickle(self,reader_inst):
-
-        if reader_inst['end_time'] == 0:
-            return
-        if os.path.exists(os.path.join(self.pickle_dir, reader_inst['id'])): 
-            print(reader_inst['id'] + " exists, don't reprocess")
-            return
-
-        job = job_stats.from_acct(reader_inst, self.tacc_stats_home, self.host_list_dir, self.acct)
-        with open(os.path.join(self.pickle_dir, job.id), 'wb') as pickle_file:
-            pickle.dump(job, pickle_file, self.pickle_prot)
-
     def run(self):
         pool = multiprocessing.Pool(processes = self.processes)
         reader = self.acct.reader(start_time=self.getdate(self.start),
-                                  end_time=self.getdate(self.end),seek=self.seek)
-        pool.map(unwrap,zip([self],reader))
+                                  end_time=self.getdate(self.end),
+                                  seek=self.seek)
 
+        partial_pickle = functools.partial(job_pickle, 
+                                           pickle_dir = self.pickle_dir, 
+                                           tacc_stats_home = self.tacc_stats_home,
+                                           host_list_dir = self.host_list_dir,
+                                           acct = self.acct,
+                                           pickle_prot = self.pickle_prot)
+        
+        pool.map(partial_pickle,reader)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run pickler for jobs')
 
-    parser.add_argument('-dir', help='Directory to store data',
-                        nargs=1, type=str)
-    parser.add_argument('-start', help='Start date',
-                        nargs=1, type=str)
-    parser.add_argument('-end', help='End date',
-                        nargs=1, type=str)
+    parser.add_argument('-dir', help='Directory to store data',type=str)
+    parser.add_argument('-start', help='Start date',type=str)
+    parser.add_argument('-end', help='End date',type=str)
     parser.add_argument('-p', help='Set number of processes',
-                        nargs=1, type=int, default=[1])
+                        type=int, default=1)
     args = parser.parse_args()
-
-    pickle_options = { 'processes'       : args.p[0],
-                       'start'           : args.start[0],
-                       'end'             : args.end[0],
-                       'pickle_dir'      : args.dir[0],
+    
+    pickle_options = { 'processes'       : args.p,
+                       'start'           : args.start,
+                       'end'             : args.end,
+                       'pickle_dir'      : args.dir,
                        'batch_system'    : cfg.batch_system,
                        'acct_path'       : cfg.acct_path,
                        'tacc_stats_home' : cfg.tacc_stats_home,
@@ -121,6 +107,6 @@ if __name__ == '__main__':
                        'host_name_ext'   : cfg.host_name_ext,
                        'seek'            : cfg.seek
                        }
-
-    pickler = job_pickler(**pickle_options)
+    print(pickle_options)
+    pickler = JobPickles(**pickle_options)
     pickler.run()
