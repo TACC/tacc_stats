7463d4b707252ddf2bb9ae259fcbe95b0284ba26
Bill Barth <bbarth@tacc.utexas.edu>
2014-08-29 10:44:07 -0500

    added packet size test

--- tacc_stats/analysis/exam/__init__.py
+++ tacc_stats/analysis/exam/__init__.py
@@ -10,3 +10,4 @@ from tacc_stats.analysis.exam.highcpi import HighCPI
 from tacc_stats.analysis.exam.highcpld import HighCPLD
 from tacc_stats.analysis.exam.c3_state import C3State
 from tacc_stats.analysis.exam.message_rate import MessageRate
+from tacc_stats.analysis.exam.packet_size import PacketSize
