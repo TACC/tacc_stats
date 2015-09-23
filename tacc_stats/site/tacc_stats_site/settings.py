e1c6409cc5aa7c0ff69cf98152aabc224758f5a0
Richard Evans <rtevans@login1.stampede.tacc.utexas.edu>
2015-09-23 11:36:36 -0500

    xalt needs to be an installed app in future django versions

--- tacc_stats/site/tacc_stats_site/settings.py
+++ tacc_stats/site/tacc_stats_site/settings.py
@@ -145,6 +145,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'tacc_stats.site.machine',
+    'tacc_stats.site.xalt',
 )
 """
 TEMPLATE_CONTEXT_PROCESSORS=(
