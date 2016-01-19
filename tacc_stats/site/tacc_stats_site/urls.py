f9891602c89ee63a586ae74caba49ab751023908
Richard Evans <rtevans@tacc.utexas.edu>
2016-01-19 14:04:33 -0600

    Update urls.py
    oops

--- tacc_stats/site/tacc_stats_site/urls.py
+++ tacc_stats/site/tacc_stats_site/urls.py
@@ -5,9 +5,9 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     #url(r'^$', 'tacc_stats_site.views.home', name='home'),
-    url(r'^$', 'tacc_stats_site.site.machine.views.dates', name='dates'),
+    url(r'^$', 'tacc_stats.site.machine.views.dates', name='dates'),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
         {'document_root': settings.MEDIA_ROOT}),                       
-    url(r'^machine/', include('tacc_stats_site.site.machine.urls', namespace="machine"),name='machine'),
+    url(r'^machine/', include('tacc_stats.site.machine.urls', namespace="machine"),name='machine'),
     url(r'^admin/', include(admin.site.urls)),
 )
