7315991a869de4a477fc6096d401ff7726167e58
root <root@staff.hikari.tacc.utexas.edu>
2017-11-28 10:29:16 -0600

    fixed benign cpuid message due to testing for amd processor

--- monitor/src/cpuid.c
+++ monitor/src/cpuid.c
@@ -49,93 +49,88 @@ int signature(processor_t p, int *n_pmcs) {
 
 
   // Determine Processor Type
-  if (strncmp(vendor, "GenuineIntel", 12) == 0) {
-    switch(p) {
-    case NEHALEM:
-      if (strncmp(sig, "06_1a", 5) == 0 || 
-	  strncmp(sig, "06_1e", 5) == 0 || 
-	  strncmp(sig, "06_1f", 5) == 0 || 
-	  strncmp(sig, "06_2e", 5) == 0) {
-	rc = 1;
-	TRACE("Nehalem %s\n", sig);
-      }
-      goto out;
-      
-    case WESTMERE:
-      if (strncmp(sig, "06_25", 5) == 0 || 
-	  strncmp(sig, "06_2c", 5) == 0 || 
-	  strncmp(sig, "06_1f", 5) == 0) {
-	rc = 1;
-	TRACE("Westmere %s\n", sig);
-      }
-      goto out;
-      
-    case IVYBRIDGE:
-      if 	(strncmp(sig, "06_3a", 5) == 0 ||
-		 strncmp(sig, "06_3e", 5) == 0) {
-	rc = 1;
-	TRACE("Ivy Bridge %s\n", sig);
-      }
-      goto out;
-      
-    case SANDYBRIDGE:
-      if (strncmp(sig, "06_2a", 5) == 0 || 
-	  strncmp(sig, "06_2d", 5) == 0) {	
-	rc = 1;
-	TRACE("Sandy Bridge %s\n", sig);
-      }
-      goto out;
-      
-    case HASWELL:
-      if (strncmp(sig, "06_3c", 5) == 0 || 
-	  strncmp(sig, "06_45", 5) == 0 || 
-	  strncmp(sig, "06_46", 5) == 0 || 
-	  strncmp(sig, "06_3f", 5) == 0) {
-	rc = 1;
-	TRACE("Haswell %s\n", sig);
-      }
-      goto out;
-      
-    case BROADWELL:
-      if (strncmp(sig, "06_3d", 5) == 0 || 
-	  strncmp(sig, "06_47", 5) == 0 ||
-	  strncmp(sig, "06_4f", 5) == 0) {
-	rc = 1;
-	TRACE("Broadwell %s %d\n", sig, (int)p);
-      }
-      goto out;
-      
-    case KNL:
-      if (strncmp(sig, "06_57", 5) == 0) {
-	rc = 1;
-	TRACE("Knights Landing %s\n", sig);
-      }
-      goto out;
-      
-    case SKYLAKE:
-      if (strncmp(sig, "06_55", 5) == 0 || 
-	  strncmp(sig, "06_4e", 5) == 0 || 
-	  strncmp(sig, "06_5e", 5) == 0) {
-	rc = 1;
-	TRACE("Skylake %s\n", sig);
-      }
-      goto out;
-
-    default:
-      ERROR("non-intel processor sig %s %p\n", sig, (int)p);
-      goto out;  
-    }
-  }
-  
   switch(p) {
+  case NEHALEM:
+    if (strncmp(sig, "06_1a", 5) == 0 || 
+	strncmp(sig, "06_1e", 5) == 0 || 
+	strncmp(sig, "06_2e", 5) == 0) {
+      rc = 1;
+      TRACE("Nehalem %s\n", sig);
+    }
+    goto out;
+      
+  case WESTMERE:
+    if (strncmp(sig, "06_25", 5) == 0 || 
+	strncmp(sig, "06_2c", 5) == 0 || 
+	strncmp(sig, "06_2f", 5) == 0) {
+      rc = 1;
+      TRACE("Westmere %s\n", sig);
+    }
+    goto out;
+      
+  case IVYBRIDGE:
+    if 	(strncmp(sig, "06_3a", 5) == 0 ||
+	 strncmp(sig, "06_3e", 5) == 0) {
+      rc = 1;
+      TRACE("Ivy Bridge %s\n", sig);
+    }
+    goto out;
+    
+  case SANDYBRIDGE:
+    if (strncmp(sig, "06_2a", 5) == 0 || 
+	strncmp(sig, "06_2d", 5) == 0) {	
+      rc = 1;
+      TRACE("Sandy Bridge %s\n", sig);
+    }
+    goto out;
+      
+  case HASWELL:
+    if (strncmp(sig, "06_3c", 5) == 0 || 
+	strncmp(sig, "06_45", 5) == 0 || 
+	strncmp(sig, "06_46", 5) == 0 || 
+	strncmp(sig, "06_3f", 5) == 0) {
+      rc = 1;
+      TRACE("Haswell %s\n", sig);
+    }
+    goto out;
+    
+  case BROADWELL:
+    if (strncmp(sig, "06_3d", 5) == 0 || 
+	strncmp(sig, "06_47", 5) == 0 ||
+	strncmp(sig, "06_4f", 5) == 0) {
+      rc = 1;
+      TRACE("Broadwell %s %d\n", sig, (int)p);
+    }
+    goto out;
+   
+  case KNL:
+    if (strncmp(sig, "06_57", 5) == 0) {
+      rc = 1;
+      TRACE("Knights Landing %s\n", sig);
+    }
+    goto out;
+      
+  case SKYLAKE:
+    if (strncmp(sig, "06_55", 5) == 0 || 
+	strncmp(sig, "06_4e", 5) == 0 || 
+	strncmp(sig, "06_5e", 5) == 0) {
+      rc = 1;
+      TRACE("Skylake %s\n", sig);
+    }
+    goto out;
+
   case AMD_10H:
     if (strncmp(vendor, "AuthenticAMD", 12) == 0) {
       rc = 1;
       TRACE("AMD_10h %s\n", sig);
     }
     goto out;
+
+  default:
+    ERROR("non-intel processor sig %s %p\n", sig, (int)p);
+    goto out;  
   }
-  
+
  out:
   return rc;
 }
