06c03083b3d920f7843a37da24e5cf2dd1ee2763
root <root@stats.frontera.tacc.utexas.edu>
2019-08-13 16:07:50 -0500

    new bokeh function signatures adhered to in views.py

--- tacc_stats/listend.py
+++ tacc_stats/listend.py
@@ -6,15 +6,22 @@ import tacc_stats.cfg as cfg
 from fcntl import flock, LOCK_EX, LOCK_NB
 
 def on_message(channel, method_frame, header_frame, body):
-    if body[0] == '$': host = body.split('\n')[1].split()[1]       
-    else: host = body.split()[2]
+
+    try:
+        message = body.decode()    
+    except: 
+        print("Unexpected error at decode:", sys.exc_info()[0])
+        return
+
+    if message[0] == '$': host = message.split('\n')[1].split()[1]       
+    else: host = message.split()[2]
 
     host_dir = os.path.join(cfg.archive_dir, host)
     if not os.path.exists(host_dir):
         os.makedirs(host_dir)
     
     current_path = os.path.join(host_dir, "current")
-    if body[0] == '$':
+    if message[0] == '$':
         if os.path.exists(current_path):
             os.unlink(current_path)
 
@@ -25,7 +32,7 @@ def on_message(channel, method_frame, header_frame, body):
             os.link(current_path, link_path)
 
     with open(current_path, 'a') as fd:
-        fd.write(body)
+        fd.write(message)
 
     channel.basic_ack(delivery_tag=method_frame.delivery_tag)
 
@@ -39,7 +46,7 @@ with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "listend_loc
     parameters = pika.ConnectionParameters(cfg.rmq_server)
     connection = pika.BlockingConnection(parameters)
     channel = connection.channel()
-    channel.basic_consume(on_message, cfg.rmq_queue)
+    channel.basic_consume(cfg.rmq_queue, on_message)
     try:
         channel.start_consuming()
     except KeyboardInterrupt:
