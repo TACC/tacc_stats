1354cabb7cfdfaaac4fef57ed21c4e6b6ba25cdf
sanga1999 <153561744+sanga1999@users.noreply.github.com>
2024-09-19 15:55:08 -0500

    Update sacct_gen.py

--- tacc_stats/dbload/sacct_gen.py
+++ tacc_stats/dbload/sacct_gen.py
@@ -1,9 +1,10 @@
 import os, sys
+sys.path.append("/home/sg99/tacc_stats")
 from datetime import timedelta, date, datetime
 from dateutil.parser import parse
-from tacc_stats import cfg
+import conf_parser as cfg
 
-acct_path = cfg.acct_path
+acct_path = cfg.get_accounting_path()
 
 def daterange(start_date, end_date):
     for n in range(int ((end_date - start_date).days)):
@@ -24,4 +25,3 @@ for single_date in daterange(start_date, end_date):
     sacct_command = "/bin/sacct -a -s CA,CD,F,NF,TO -P -X -S " + single_date.strftime("%Y-%m-%d") + " -E " + (single_date + timedelta(1)).strftime("%Y-%m-%d") +" -o JobID,User,Account,Start,End,Submit,Partition,TimeLimit,JobName,State,NNodes,ReqCPUS,NodeList > " + file_name  
     print(sacct_command)
     os.system(sacct_command)
-
