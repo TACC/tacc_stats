b7de4a33694e3bcd4a68183677e7e3c280406934
Stephen Lien Harrell <sharrell@tacc.utexas.edu>
2025-01-24 13:55:52 -0500

    Fix path to binary location

--- services-conf/rsync_acct.sh
+++ services-conf/rsync_acct.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while true
 do
-    /bin/rsync -avq -e 'ssh -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no' sharrell@staff.stampede3.tacc.utexas.edu:/home1/01623/sharrell/s3_acct_logs/* /hpcperfstats/accounting/
+    /usr/bin/rsync -avq -e 'ssh -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no' sharrell@staff.stampede3.tacc.utexas.edu:/home1/01623/sharrell/s3_acct_logs/* /hpcperfstats/accounting/
     sleep 3600
 done
